cc66a3b7c302d788cf456f516ab6e353d7081d03
mtali <emmanuelmtali@yahoo.com>
2023-09-07 00:19:40 +0300

    Add pokemons feature module

---
+++ feature/pokemons/build.gradle.kts
@@ -0,0 +1,33 @@
+import com.colisa.monsterdex.Configuration
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.colisa.mosterdex.feature.pokemons"
+    compileSdk = Configuration.compileSdk
+
+    defaultConfig {
+        minSdk = Configuration.minSdk
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+}
+
+dependencies {
+
+    // Hilt (DI)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+}
