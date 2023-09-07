9a689e10341c440fcd14f19f3043574ff31a3213
mtali <emmanuelmtali@yahoo.com>
2023-09-07 21:25:50 +0300

    Add edge-to-edge on main activity

--- app/src/main/kotlin/com/colisa/monsterdex/MainActivity.kt
+++ app/src/main/kotlin/com/colisa/monsterdex/MainActivity.kt
@@ -3,6 +3,7 @@ package com.colisa.monsterdex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.colisa.monsterdex.ui.MonsterdexApp
 import com.colisa.mosterdex.core.design_system.theme.MonsterdexTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MonsterdexTheme {
                 MonsterdexApp()
