e77abd48a3dcb0c27877bd76f8a0665893b10c1d
mtali <emmanuelmtali@yahoo.com>
2023-09-07 14:43:33 +0300

    Fix import errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,13 @@ android {
 }
 
 dependencies {
+
+    implementation(project(":core:design-system"))
+
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
+
     implementation(platform(libs.compose.bom))
     implementation(libs.ui)
     implementation(libs.ui.graphics)
--- app/src/main/kotlin/com/colisa/monsterdex/MainActivity.kt
+++ app/src/main/kotlin/com/colisa/monsterdex/MainActivity.kt
@@ -3,44 +3,17 @@ package com.colisa.monsterdex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.colisa.mosterdex.core.design_system.theme.MonsterdexTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            com.colisa.mosterdex.core.design_system.theme.MonsterdexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MonsterdexTheme {
+                Text(text = "Mtali")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    com.colisa.mosterdex.core.design_system.theme.MonsterdexTheme {
-        Greeting("Android")
     }
 }
