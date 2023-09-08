2ea147351d3bd87a3ee0bad8a4db436c65930199
mtali <emmanuelmtali@yahoo.com>
2023-09-08 23:43:05 +0300

    Add todo for refactoring parseOffsetFromUrl method

--- core/network/src/main/kotlin/com/colisa/monsterdex/core/network/UrlUtils.kt
+++ core/network/src/main/kotlin/com/colisa/monsterdex/core/network/UrlUtils.kt
@@ -2,6 +2,7 @@ package com.colisa.monsterdex.core.network
 
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 
+// TODO: Best to make pass in the key
 fun parseOffsetFromUrl(url: String?): Int? {
     url ?: return null
     val httpUrl = url.toHttpUrlOrNull()
